6c220c74b867ab61adafd16819795ec9438d1844
skochdev <36549367+skochdev@users.noreply.github.com>
2022-05-01 21:40:02 +0000

    Deploying to gh-pages from @ skochdev/team10-js-project@402fa0db4397228357465d8cf53126a9b07bfdd0 🚀

--- src.532e7203.js
+++ src.532e7203.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team10-js-project/src.687ca9b5.js.map
+//# sourceMappingURL=/team10-js-project/src.532e7203.js.map
