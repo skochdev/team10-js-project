ac5c04430a6b97c0a0bfb5a1eba59b1696bebe74
Bohdan Skochii <b.skochii@gmail.com>
2022-05-05 02:42:23 +0300

    leave spinner for the timebeing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './sass/main.scss';
 import getRefs from './js/get-refs';
 import renderSpinner from './js/spinner';
+
+const refs = getRefs();
+
+refs.searchBtnRef.addEventListener('click', onSearchClick);
+
+function onSearchClick() {
+  renderSpinner(refs.mainRef);
+}
