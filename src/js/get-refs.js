47cff0e7eb38eddd0ff17091b8b5b7b7259b2454
Bohdan Skochii <b.skochii@gmail.com>
2022-05-05 01:56:53 +0300

    add get-refs function to store all refs.

---
+++ src/js/get-refs.js
@@ -0,0 +1,19 @@
+/*  Создал Богдан тим-лид
+
+  Тут храним все рефы. Импортируете куда вам нужно, добавляйте рефы если нужно еще,
+  делаете себе в файле переменную 
+
+      const refs = getRefs();
+
+  и далее используете по стандарту refs.searchBtnRef.addEveventListener()   и тд
+
+*/
+
+export default function getRefs() {
+  return {
+    searchBtnRef: document.querySelector('[data-action="search-button"]'),
+    mainRef: document.querySelector('main'),
+    homeRef: document.querySelector('[data-link="home"]'),
+    myLibraryRef: document.querySelector('data-link="my-library"'),
+  };
+}
