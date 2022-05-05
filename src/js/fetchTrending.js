77fe3319b7a12b495530d2d77e087ef20e3e6a87
Lilia Lomaka <llomaka80@gmail.com>
2022-05-05 19:44:56 +0300

    Видалена зайва функція

--- src/js/fetchTrending.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function fetchTrending() {
-  const { data } = await axios(`https://api.themoviedb.org/3/trending/movie/day?api_key=fdf0e898687a376156944fbb1ab25196`);
-  return data;
-}
