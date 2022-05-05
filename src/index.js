21912fb6fa51c7c418327440841ec5023daf7236
Anton Honcharov <antonhoncharovv@gmail.com>
2022-05-05 22:56:16 +0300

    add macrkup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import './sass/main.scss';
 import saveGenresToLocalStorage from './js/saveGenresToLocalStorage';
+import fetchPopularMovies from './js/fetchPopularMovies';
+import renderTrending from './js/renderTrending';
+import getRefs from './js/get-refs';
+
+const REFS = getRefs();
 
 saveGenresToLocalStorage();
+
+fetchPopularMovies().then(data => {
+    return renderTrending(REFS.galleryRef, data, REFS.parsedGenresRef)
+})
