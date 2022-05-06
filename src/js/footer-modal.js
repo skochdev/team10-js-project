139353f5835b2e846f5c8b564433be2a73598222
Bohdan Skochii <b.skochii@gmail.com>
2022-05-06 15:43:06 +0300

    add names to cards for footer modal.

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -10,7 +10,7 @@ function onFooterModalOpen() {
   REFS.footerCloseBtnRef.addEventListener('click', onFooterModalClose);
   window.addEventListener('keydown', onEscape);
   REFS.footerModalBackdrop.classList.remove('is-hidden');
-  document.body.style.overflow = 'hidden';
+  setBodyOverflow('initial');
 }
 
 function onFooterModalClose() {
@@ -18,13 +18,25 @@ function onFooterModalClose() {
   window.removeEventListener('keydown', onEscape);
   REFS.footerCloseBtnRef.removeEventListener('click', onFooterModalClose);
   REFS.footerModalBackdrop.classList.add('is-hidden');
-  document.body.style.overflow = 'auto';
+  setBodyOverflow('initial');
 }
+
+// Закрываем мобильное меню на более широких экранах
+// в случае изменения ориентации устройства.
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  REFS.footerModalBackdrop.classList.add('is-hidden');
+  setBodyOverflow('initial');
+});
 
 function onFooterModalBackdropClick(e) {
   if (e.target === e.currentTarget || e.code === 'Escape') {
     onFooterModalClose();
   }
+}
+
+function setBodyOverflow(value) {
+  document.body.style.overflow = value;
 }
 
 function onEscape(e) {
