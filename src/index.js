bfb4477a729cbadaf731d598b03f7b0ffe36f778
Lilia Lomaka <llomaka80@gmail.com>
2022-05-06 14:09:12 +0300

    Перевірка наявності жанрів у сховищі

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './sass/main.scss';
 import getRefs from './js/get-refs';
+import fetchGenres from './js/fetchGenres';
 import saveGenresToLocalStorage from './js/saveGenresToLocalStorage';
 import renderTrending from './js/renderTrending';
 import fetchPopularMovies from './js/fetchPopularMovies';
 
 const refs = getRefs();
 saveGenresToLocalStorage();
-const genres = JSON.parse(localStorage.getItem('genre_ids'));
+const genres = localStorage.getItem('genre_ids') ? JSON.parse(localStorage.getItem('genre_ids')) : fetchGenres();
 fetchPopularMovies(1).then(response => {
   renderTrending(refs.gallery, response.results, genres);
   localStorage.setItem('trending', JSON.stringify(response));
