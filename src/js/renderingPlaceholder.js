1689154880a0efab7724bdb56439ab3f982939c1
Bohdan Skochii <b.skochii@gmail.com>
2022-05-09 17:21:31 +0300

    add padding to poster moviedb

--- src/js/renderingPlaceholder.js
+++ src/js/renderingPlaceholder.js
@@ -5,7 +5,7 @@ const refs = getRefs();
 
 export default function renderingPlaceholder() {
   const markup = `<li class="movie__item">
-    <a href="https://www.themoviedb.org/" target="_blank" class="movie__poster">
+    <a href="https://www.themoviedb.org/" target="_blank" class="movie__link">
     <img class="movie__poster js-placeholder" src="${photo}" alt="TMDB"/>
       <div class="movie__caption">
       <h2 class="movie__title">This product uses the TMDB API but is not endorsed or certified by TMDB.</h2>
