bdf3839bf5ea12435bdf923f2477d6a7f4d5c905
Anton Honcharov <antonhoncharovv@gmail.com>
2022-05-09 10:44:25 +0300

    add some changes

--- src/js/onLoaderVisible.js
+++ src/js/onLoaderVisible.js
@@ -3,5 +3,5 @@ import getRefs from './get-refs';
 const REFS = getRefs();
 
 export default function onLoaderVisible() { 
-   REFS.loader.classList.remove('visually-hidden'); 
+  REFS.loader.classList.remove('visually-hidden'); 
 };  
