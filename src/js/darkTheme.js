f9ab1be15f3bcb4d9d82ecb858605aef4d9e8e97
Vitalii Peresoliak <vitalik21022@gmail.com>
2022-05-12 18:08:50 +0300

    Fixed dark mode

--- src/js/darkTheme.js
+++ src/js/darkTheme.js
@@ -3,13 +3,15 @@ import getRefs from './get-refs';
 
 const refs = getRefs();
 const darkmode = new Darkmode();
+chosenTheme();
 
 refs.changeTheme = document.querySelector('.dark-mode');
 refs.changeTheme.addEventListener('click', onDarkMode);
 
 export default function onDarkMode() {
-    refs.changeTheme.classList.add(darkmode.toggle());
 
+    refs.changeTheme.classList.add(darkmode.toggle());
+    
     if (darkmode.isActivated()) {
         document.getElementById("dark-toggle").checked = true;
     }
@@ -17,3 +19,14 @@ export default function onDarkMode() {
         document.getElementById("dark-toggle").checked = false;
     }
 }
+
+
+function chosenTheme() {
+    if (localStorage.getItem('darkmode') === 'true') {
+        document.getElementById("dark-toggle").checked = true;
+    }
+    if (localStorage.getItem('darkmode') === 'false') {
+        document.getElementById("dark-toggle").checked = false;
+    }
+}
+
