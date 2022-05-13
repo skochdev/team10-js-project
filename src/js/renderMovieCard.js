b38a01f21ea0574781a0442c8715c3be1661a19d
Lilia Lomaka <llomaka80@gmail.com>
2022-05-13 09:34:28 +0300

    Відновлення розмітки картки, виконання рейтингу

--- src/js/renderMovieCard.js
+++ src/js/renderMovieCard.js
@@ -2,7 +2,7 @@ import getGenres from './getGenres';
 import fetchGenres from './fetchGenres';
 import empty from '../images/no-image-placeholder.svg';
 
-// картка фільму головної сторінки, передається об'єкт фільму та у разі потреби mode='library'
+// картка фільму головної сторінки, передається об'єкт фільму
 export default function renderMovieCard(film) {
   const genres = localStorage.getItem('genre_ids') ? JSON.parse(localStorage.getItem('genre_ids')) : fetchGenres();
   let genre = '';
@@ -34,20 +34,18 @@ export default function renderMovieCard(film) {
     poster = 'https://image.tmdb.org/t/p/w342' + film.poster_path;
   }
   return `<li class="movie__item" data-id="${film.id}">
-  <div class="movie__card">    
-    <a href="#" class="movie__link" data-id="${film.id}">            
-      <img class="movie__poster"
-        src="${poster}"
-        alt="${film.title} Poster"
-        loading="lazy"        
-      />
-      <span class="movie__vote">${film.vote_average.toFixed(1)}</span>
-      </div>
-      <div class="movie__caption">
-        <h2 class="movie__title">${film.title}</h2>
-        <p class="movie__genre">${genre} | ${film.release_date.slice(0, 4)}        
-        </p>        
-      </div>    
-    </a>
+    <div class="movie__card">
+      <a href="#" class="movie__link" data-id="${film.id}">
+        <img class="movie__poster"
+          src="${poster}"
+          alt="${film.title} Poster"
+          loading="lazy"
+        />
+        <div class="movie__caption">
+          <h2 class="movie__title">${film.title}</h2>
+          <p class="movie__genre">${genre} | ${film.release_date.slice(0, 4)} <span class="movie__vote movie__vote--position">${film.vote_average.toFixed(1)}</span></p>
+        </div>
+      </a>
+    </div>
   </li>`;
 }
